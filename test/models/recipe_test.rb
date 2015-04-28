59769c3ccfcb4033e05fd280fa91f4e1bc9ef919
Melissa Nemeth <melissanemeth1@gmail.com>
2015-04-28 12:56:20 +0000

    Fixed errors in db migration files and recipe_test file

--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -4,11 +4,11 @@ class RecipeTest < ActiveSupport::TestCase
   
   def setup
     @chef = Chef.create(chefname: "bob", email: "bob@example.com")
-    @recipe = @chef.recipes.build(name: "Eggplant Parmesean", summary: "An excellent vegan take on the old Italian classic!",
-                        description: "Coat eggplant in flour and lightly fry on each side. Layer with tomato sauce and vegan Parmesean, bake at 350 for 20 minutes.")
+    @recipe = @chef.recipes.build(name: "chicken parm", summary: "this is the best chicken parm recipe ever",
+              description: "heat oil, add onions, add tomato sauce, add chicken, cook for 20 minutes")
   end
   
-  test "Recipe should be valid" do 
+  test "recipe should be valid" do
     assert @recipe.valid?
   end
   
@@ -17,52 +17,50 @@ class RecipeTest < ActiveSupport::TestCase
     assert_not @recipe.valid?
   end
   
-  test "Name should be present" do
+  
+  test "name should be present" do
     @recipe.name = " "
     assert_not @recipe.valid?
   end
   
-  test "Name length should not be too long" do
+  test "name length should not be too long" do
     @recipe.name = "a" * 101
     assert_not @recipe.valid?
   end
   
-  test "Name length should not be too short" do
+  test "name length should not be too short" do
     @recipe.name = "aaaa"
     assert_not @recipe.valid?
   end
   
-  test "Summary must be present" do
+  test "summary should be present" do
     @recipe.summary = " "
     assert_not @recipe.valid?
   end
   
-  test "Summary length should not be too long" do
+  test "summary length should not be too long" do
     @recipe.summary = "a" * 151
     assert_not @recipe.valid?
   end
   
-  test "Summary length should not be too short" do
+  test "summary length should not be too short" do
     @recipe.summary = "a" * 9
     assert_not @recipe.valid?
   end
   
-  test "Description must be present" do
+  test "description must be present" do
     @recipe.description = " "
     assert_not @recipe.valid?
   end
   
-  test "Description should not be too long" do
+  test "description should not be too long" do
     @recipe.description = "a" * 1501
     assert_not @recipe.valid?
   end
-
-  test "Description should not be too short" do
+  
+  test "description should not be too short" do
     @recipe.description = "a" * 19
     assert_not @recipe.valid?
   end
-  
-  
-  
   
 end
